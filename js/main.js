88fae213811d99cc53afd07637eca4a43bae3e10
Adilzhan Sarsen <lolrdmen@gmail.com>
2021-05-19 04:26:31 +0600

    update pwa

--- js/main.js
+++ js/main.js
@@ -3,14 +3,7 @@ let selectedRelay
 const sensorsList = ['temperature', 'humidity', 'moisture']
 
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register("sw.js")
-        .then(function (registration) {
-            console.log("success load");
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
+    navigator.serviceWorker.register("sw.js")
 }
 
 
